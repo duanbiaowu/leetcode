63eef608fce984e479cb72dd941e032c7edcdd1e
duanbiaowu <codean.net@gmail.com>
2021-07-19 07:38:34 +0800

    [feat] 0005.Longest-Palindromic-Substring dp solution

--- 0005.Longest-Palindromic-Substring/Solution.go
+++ 0005.Longest-Palindromic-Substring/Solution.go
@@ -22,24 +22,81 @@ package leetcode
 //}
 
 // 中心扩展(利用回文串性质)
+//func longestPalindrome(s string) string {
+//	if len(s) <= 1 {
+//		return s
+//	}
+//
+//	start, end := 0, 0
+//	for i := 0; i < len(s); i++ {
+//		left1, right1 := expandAroundCenter(s, i, i)	// 单个字符向两边扩展
+//		left2, right2 := expandAroundCenter(s, i, i+1)	// 相邻字符向两边扩展
+//		if right1-left1 > end-start {
+//			start, end = left1, right1
+//		}
+//		if right2-left2 > end-start {
+//			start, end = left2, right2
+//		}
+//	}
+//
+//	return s[start : end+1]
+//}
+
+// DP
 func longestPalindrome(s string) string {
 	if len(s) <= 1 {
 		return s
 	}
 
-	start, end := 0, 0
-	for i := 0; i < len(s); i++ {
-		left1, right1 := expandAroundCenter(s, i, i)
-		left2, right2 := expandAroundCenter(s, i, i+1)
-		if right1-left1 > end-start {
-			start, end = left1, right1
-		}
-		if right2-left2 > end-start {
-			start, end = left2, right2
+	// 回文起始位置
+	begin := 0
+	// 回文长度, 通过起始位置+长度, 可以直接返回结果
+	maxLen := 1
+	n := len(s)
+
+	// dp[i][j] 表示 s[i..j] 是否是回文串
+	dp := [][]bool{}
+	for i := 0; i < n; i++ {
+		dp = append(dp, make([]bool, n+1))
+	}
+
+	// 初始化：所有长度为 1 的子串都是回文串
+	for i := 0; i < n; i++ {
+		dp[i][i] = true
+	}
+
+	// 递推开始
+	// 先枚举子串长度
+	for left := 2; left <= n; left++ {
+		// 枚举左边界
+		for i := 0; i < n; i++ {
+			// 由 left 和 i 可以确定右边界，即 j - i + 1 = left
+			j := left + i - 1
+			if j >= n {
+				break
+			}
+
+			if s[i] != s[j] {
+				dp[i][j] = false
+			} else {
+				// 边界情况, "aa" "aba" ...
+				if j-i < 3 {
+					dp[i][j] = true
+				} else {
+					dp[i][j] = dp[i+1][j-1]
+				}
+			}
+
+			// 只要 dp[i][j] == true 成立，就表示子串 s[i..j] 是回文
+			// 更新回文长度和起始位置
+			if dp[i][j] && j-i+1 > maxLen {
+				maxLen = j - i + 1
+				begin = i
+			}
 		}
 	}
 
-	return s[start : end+1]
+	return s[begin : begin+maxLen]
 }
 
 func expandAroundCenter(s string, left, right int) (int, int) {
