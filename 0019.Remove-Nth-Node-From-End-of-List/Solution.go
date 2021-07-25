6295ec8aecbcac32ee3f71fd78a832a25298ffa7
duanbiaowu <codean.net@gmail.com>
2021-07-25 14:43:43 +0800

    [feat] 0019.Remove-Nth-Node-From-End-of-List fast-slow-pointer solution

--- 0019.Remove-Nth-Node-From-End-of-List/Solution.go
+++ 0019.Remove-Nth-Node-From-End-of-List/Solution.go
@@ -14,17 +14,38 @@ func getLength(head *ListNode) int {
 	return length
 }
 
+// simple solution
+//func removeNthFromEnd(head *ListNode, n int) *ListNode {
+//	if n <= 0 {
+//		return head
+//	}
+//
+//	dump := &ListNode{0, head}
+//	length := getLength(head)
+//	cur := dump
+//	for i := 0; i < length-n+1; i++ {
+//		cur = cur.Next
+//	}
+//	cur.Next = cur.Next.Next
+//	return dump.Next
+//}
+
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	if n <= 0 {
 		return head
 	}
 
 	dump := &ListNode{0, head}
-	length := getLength(head)
-	cur := dump
-	for i := 0; i < length-n+1; i++ {
-		cur = cur.Next
+	fast, slow := head, dump
+	for i := 0; i < n && fast != nil; i++ {
+		fast = fast.Next
 	}
-	cur.Next = cur.Next.Next
+
+	for fast != nil {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	slow.Next = slow.Next.Next
+
 	return dump.Next
 }
