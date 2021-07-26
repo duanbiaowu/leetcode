a655f496b93cc0ef71f8ba59a98e5de970a58de3
duanbiaowu <codean.net@gmail.com>
2021-07-26 22:46:45 +0800

    [feat] 0021.Merge-Two-Sorted-Lists simple solution

---
+++ 0021.Merge-Two-Sorted-Lists/Solution.go
@@ -0,0 +1,30 @@
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	dummy := &ListNode{Val: 0}
+	cur := dummy
+
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			cur.Next = l1
+			l1 = l1.Next
+		} else {
+			cur.Next = l2
+			l2 = l2.Next
+		}
+		cur = cur.Next
+	}
+
+	if l1 == nil {
+		cur.Next = l2
+	} else {
+		cur.Next = l1
+	}
+
+	return dummy.Next
+}
---
+++ 0021.Merge-Two-Sorted-Lists/Solution_test.go
@@ -0,0 +1,66 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_mergeTwoLists(t *testing.T) {
+	type args struct {
+		l1 *ListNode
+		l2 *ListNode
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			"test two empty listNodes",
+			args{nil, nil},
+			nil,
+		},
+
+		{
+			"No1. test an empty listNodes and a non-empty listNodes",
+			args{convertArrayToListNodes([]int{1}), nil},
+			convertArrayToListNodes([]int{1}),
+		},
+		{
+			"No2. test an empty listNodes and a non-empty listNodes",
+			args{nil, convertArrayToListNodes([]int{2})},
+			convertArrayToListNodes([]int{2}),
+		},
+
+		{
+			"No1. test two non-empty listNodes",
+			args{convertArrayToListNodes([]int{1}), convertArrayToListNodes([]int{2})},
+			convertArrayToListNodes([]int{1, 2}),
+		},
+		{
+			"No2. test two non-empty listNodes",
+			args{convertArrayToListNodes([]int{1, 3, 5}), convertArrayToListNodes([]int{2, 4, 6})},
+			convertArrayToListNodes([]int{1, 2, 3, 4, 5, 6}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mergeTwoLists(tt.args.l1, tt.args.l2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("mergeTwoLists() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func convertArrayToListNodes(nums []int) *ListNode {
+	dummy := &ListNode{}
+	cur := dummy
+
+	for _, val := range nums {
+		cur.Next = &ListNode{Val: val}
+		cur = cur.Next
+	}
+
+	return dummy.Next
+}
