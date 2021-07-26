a02e9a7d924206d2ba5d3ebb07b50af9e30e3358
duanbiaowu <codean.net@gmail.com>
2021-07-26 22:53:28 +0800

    [feat] 0021.Merge-Two-Sorted-Lists recursive solution

--- 0021.Merge-Two-Sorted-Lists/Solution.go
+++ 0021.Merge-Two-Sorted-Lists/Solution.go
@@ -28,3 +28,18 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 
 	return dummy.Next
 }
+
+func mergeTwoListsRecursively(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoListsRecursively(l1.Next, l2)
+		return l1
+	}
+	l2.Next = mergeTwoListsRecursively(l1, l2.Next)
+	return l2
+}
