c146c2019356517ba8605aa65494aff3be39a406
duanbiaowu <codean.net@gmail.com>
2021-07-28 17:27:26 +0800

    [feat] 0053.Maximum-Subarray dp solution

--- 0053.Maximum-Subarray/Solution.go
+++ 0053.Maximum-Subarray/Solution.go
@@ -1,18 +1,38 @@
 package leetcode
 
+// simple solution
+//func maxSubArray(nums []int) int {
+//	if len(nums) == 0 {
+//		return 0
+//	}
+//	res, sum := nums[0], nums[0]
+//	for i := 1; i < len(nums); i++ {
+//		if sum > 0 {
+//			sum += nums[i]
+//		} else {
+//			sum = nums[i]
+//		}
+//		if sum > res {
+//			res = sum
+//		}
+//	}
+//	return res
+//}
+
+// dp solution
 func maxSubArray(nums []int) int {
 	if len(nums) == 0 {
 		return 0
 	}
-	res, sum := nums[0], nums[0]
+
+	res := nums[0]
 	for i := 1; i < len(nums); i++ {
-		if sum > 0 {
-			sum += nums[i]
-		} else {
-			sum = nums[i]
+		// update max value of nums[0] - nums[i]
+		if nums[i]+nums[i-1] > nums[i] {
+			nums[i] += nums[i-1]
 		}
-		if sum > res {
-			res = sum
+		if nums[i] > res {
+			res = nums[i]
 		}
 	}
 	return res
