346b0de37f3ed2a8fccb4073971ca40f59c14e7a
duanbiaowu <codean.net@gmail.com>
2021-07-29 14:03:34 +0800

    [refactor] 0088.Merge-Sorted-Array solution comments

--- 0088.Merge-Sorted-Array/Solution.go
+++ 0088.Merge-Sorted-Array/Solution.go
@@ -16,7 +16,9 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 
 	// 题目假设 nums1 的空间大小等于 m + n
 	// 这样它就有足够的空间保存来自 nums2 的元素
-	// 所以这里 slowN1 不会越过边界
+	// 所以这里 slowN1 存在两种情况
+	// 	(1) >  0 说明两个数组中最小的元素在 nums1，无需调整
+	//  (2) == 0 说明两个数组中最小的元素在 nums2，仅调整 nums2 中剩余元素
 	for slowN2 >= 0 {
 		nums1[fast] = nums2[slowN2]
 		fast--
