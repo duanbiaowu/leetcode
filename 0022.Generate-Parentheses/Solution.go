68ead8072d49bdae6e106948c51ba525ecfbeb77
duanbiaowu <codean.net@gmail.com>
2021-07-30 21:33:56 +0800

    [feat] 0022.Generate-Parentheses/Solution.go backtrack solution

--- 0022.Generate-Parentheses/Solution.go
+++ 0022.Generate-Parentheses/Solution.go
@@ -1,14 +1,14 @@
 package leetcode
 
 // simple solution
-func generateParenthesis(n int) []string {
-	if n <= 0 {
-		return []string{}
-	}
-	var res []string
-	generateAll(make([]byte, n*2), 0, &res)
-	return res
-}
+//func generateParenthesis(n int) []string {
+//	if n <= 0 {
+//		return []string{}
+//	}
+//	var res []string
+//	generateAll(make([]byte, n*2), 0, &res)
+//	return res
+//}
 
 func generateAll(current []byte, index int, res *[]string) {
 	if index == len(current) {
@@ -36,4 +36,28 @@ func isValid(current []byte) bool {
 		}
 	}
 	return balance == 0
+}
+
+// backtrack solution
+func generateParenthesis(n int) []string {
+	if n <= 0 {
+		return []string{}
+	}
+	var res []string
+	backtrack(n, n, "", &res)
+	return res
+}
+
+func backtrack(left, right int, str string, res *[]string) {
+	// 左右两边同时结束，说明对称
+	if left == 0 && right == 0 {
+		*res = append(*res, str)
+		return
+	}
+	if left > 0 {
+		backtrack(left-1, right, str+"(", res)
+	}
+	if right > 0 && right > left {
+		backtrack(left, right-1, str+")", res)
+	}
 }
