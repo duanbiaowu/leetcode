d64a949d4392daea2a452e18d9c965821a94b342
duanbiaowu <codean.net@gmail.com>
2021-08-03 22:25:00 +0800

    [feat] 0203.Remove-Linked-List-Elements/Solution.go recursive solution

--- 0203.Remove-Linked-List-Elements/Solution.go
+++ 0203.Remove-Linked-List-Elements/Solution.go
@@ -17,3 +17,14 @@ func removeElements(head *ListNode, val int) *ListNode {
 
 	return dummy.Next
 }
+
+func removeElementsRecursively(head *ListNode, val int) *ListNode {
+	if head == nil {
+		return head
+	}
+	head.Next = removeElementsRecursively(head.Next, val)
+	if head.Val == val {
+		return head.Next
+	}
+	return head
+}
