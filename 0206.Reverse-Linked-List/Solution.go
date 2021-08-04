9ce678949facbd711b4d4e54cbe316ad477886a9
duanbiaowu <codean.net@gmail.com>
2021-08-04 13:10:52 +0800

    [feat] 0206.Reverse-Linked-List recursive solution

--- 0206.Reverse-Linked-List/Solution.go
+++ 0206.Reverse-Linked-List/Solution.go
@@ -28,3 +28,14 @@ func reverseListForGolang(head *ListNode) *ListNode {
 
 	return prev
 }
+
+func reverseListRecursively(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	newHead := reverseListRecursively(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return newHead
+}
