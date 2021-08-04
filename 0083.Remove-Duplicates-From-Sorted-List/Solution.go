e11e15e47f2c7170cbb5cea675d52bf699fefcdf
duanbiaowu <codean.net@gmail.com>
2021-08-04 13:18:59 +0800

    [feat] 0083.Remove-Duplicates-From-Sorted-List iterative solution

--- 0083.Remove-Duplicates-From-Sorted-List/Solution.go
+++ 0083.Remove-Duplicates-From-Sorted-List/Solution.go
@@ -15,3 +15,20 @@ func deleteDuplicates(head *ListNode) *ListNode {
 	}
 	return head
 }
+
+func deleteDuplicatesIteratively(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	cur := head
+	for cur.Next != nil {
+		if cur.Val == cur.Next.Val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+
+	return head
+}
