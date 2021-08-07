3a174c988909af690fa05b127c1aa206079f3347
duanbiaowu <codean.net@gmail.com>
2021-08-07 09:04:59 +0800

    [feat] 0144.Binary-Tree-Preorder-Traversal iteratively solution

--- 0144.Binary-Tree-Preorder-Traversal/Solution.go
+++ 0144.Binary-Tree-Preorder-Traversal/Solution.go
@@ -10,6 +10,23 @@ func preorderTraversal(root *TreeNode) []int {
 	return res
 }
 
+func preorderTraversalIteratively(root *TreeNode) []int {
+	var res []int
+	var stack []*TreeNode
+	node := root
+	for node != nil || len(stack) > 0 {
+		for node != nil {
+			res = append(res, node.Val)
+			stack = append(stack, node)
+			node = node.Left
+		}
+		node = stack[len(stack)-1].Right
+		stack = stack[:len(stack)-1]
+	}
+
+	return res
+}
+
 func traversalRecursively(root *TreeNode, res *[]int) {
 	if root != nil {
 		*res = append(*res, root.Val)
