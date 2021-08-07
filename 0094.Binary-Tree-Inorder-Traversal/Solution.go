c092aaaeb0fff0fa473a9f17591585f082630936
duanbiaowu <codean.net@gmail.com>
2021-08-07 09:32:44 +0800

    [feat] 0094.Binary-Tree-Inorder-Traversal/Solution.go iteratively solution

--- 0094.Binary-Tree-Inorder-Traversal/Solution.go
+++ 0094.Binary-Tree-Inorder-Traversal/Solution.go
@@ -10,6 +10,24 @@ func inorderTraversal(root *TreeNode) []int {
 	return res
 }
 
+func inorderTraversalIteratively(root *TreeNode) []int {
+	var res []int
+	var stack []*TreeNode
+	node := root
+	for node != nil || len(stack) > 0 {
+		for node != nil {
+			stack = append(stack, node)
+			node = node.Left
+		}
+		node = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, node.Val)
+		node = node.Right
+	}
+
+	return res
+}
+
 func inorderTraversalRecursively(root *TreeNode, res *[]int) {
 	if root != nil {
 		inorderTraversalRecursively(root.Left, res)
