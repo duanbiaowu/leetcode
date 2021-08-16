115dc7126a29b575000f0a22ddcbd402d2177ae2
duanbiaowu <codean.net@gmail.com>
2021-08-17 07:57:03 +0800

    [feat] 0101.Symmetric-Tree bfs solution

--- 0101.Symmetric-Tree/Solution.go
+++ 0101.Symmetric-Tree/Solution.go
@@ -17,3 +17,30 @@ func isMirror(p, q *TreeNode) bool {
 	}
 	return p.Val == q.Val && isMirror(p.Left, q.Right) && isMirror(p.Right, q.Left)
 }
+
+func isSymmetricBFS(root *TreeNode) bool {
+	var queue []*TreeNode
+	queue = append(queue, root)
+	queue = append(queue, root)
+
+	for len(queue) > 0 {
+		p, q := queue[0], queue[1]
+		queue = queue[2:]
+		if p == nil && q == nil {
+			continue
+		}
+		if p == nil || q == nil {
+			return false
+		}
+		if p.Val != q.Val {
+			return false
+		}
+
+		queue = append(queue, p.Left)
+		queue = append(queue, q.Right)
+		queue = append(queue, p.Right)
+		queue = append(queue, q.Left)
+	}
+
+	return true
+}
