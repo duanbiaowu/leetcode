e55919cc84323262b96d013ef48017aeded846be
duanbiaowu <codean.net@gmail.com>
2021-08-19 07:57:19 +0800

    [refactor] 0106.Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal test cases

--- 0106.Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/Solution_test.go
+++ 0106.Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/Solution_test.go
@@ -33,6 +33,36 @@ func Test_buildTree(t *testing.T) {
 				Left: &TreeNode{Val: 2},
 			},
 		},
+		{
+			"test-4",
+			args{[]int{1, 2}, []int{2, 1}},
+			&TreeNode{
+				Val:   1,
+				Right: &TreeNode{Val: 2},
+			},
+		},
+		{
+			"test-5",
+			args{[]int{2, 1, 3}, []int{2, 3, 1}},
+			&TreeNode{
+				Val:   1,
+				Left:  &TreeNode{Val: 2},
+				Right: &TreeNode{Val: 3},
+			},
+		},
+		{
+			"test-6",
+			args{[]int{9, 3, 15, 20, 7}, []int{9, 15, 7, 20, 3}},
+			&TreeNode{
+				Val:  3,
+				Left: &TreeNode{Val: 9},
+				Right: &TreeNode{
+					Val:   20,
+					Left:  &TreeNode{Val: 15},
+					Right: &TreeNode{Val: 7},
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
