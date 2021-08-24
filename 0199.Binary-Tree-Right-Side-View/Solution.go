fe9cbf8ba30ac7912a84c24afa31cac7afa6332a
duanbiaowu <codean.net@gmail.com>
2021-08-24 13:50:29 +0800

    [feat] 0199.Binary-Tree-Right-Side-View DFS solution

--- 0199.Binary-Tree-Right-Side-View/Solution.go
+++ 0199.Binary-Tree-Right-Side-View/Solution.go
@@ -28,3 +28,22 @@ func rightSideView(root *TreeNode) []int {
 
 	return res
 }
+
+// 前序遍历反向操作: 根结点 -> 右子树 -> 左子树
+// 保证每层都是最先访问最右边的节点
+func rightSideViewDFS(root *TreeNode) []int {
+	var res []int
+	dfs(root, 0, &res)
+	return res
+}
+
+func dfs(root *TreeNode, depth int, res *[]int) {
+	if root == nil {
+		return
+	}
+	if depth == len(*res) {
+		*res = append(*res, root.Val)
+	}
+	dfs(root.Right, depth+1, res)
+	dfs(root.Left, depth+1, res)
+}
