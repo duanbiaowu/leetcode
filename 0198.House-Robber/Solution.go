5965b62c60b08561975bbb69280c9413b3bd51b1
duanbiaowu <codean.net@gmail.com>
2021-08-24 15:25:22 +0800

    [refactor] 0198.House-Robber/Solution.go optimize dp array

--- 0198.House-Robber/Solution.go
+++ 0198.House-Robber/Solution.go
@@ -18,6 +18,24 @@ func rob(nums []int) int {
 	return dp[n-1]
 }
 
+// 优化DP数组为两个变量: 类似于 Fib数列
+func rob2(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 1
+	}
+	if n == 1 {
+		return nums[0]
+	}
+
+	first := nums[0]
+	second := max(nums[0], nums[1])
+	for i := 2; i < n; i++ {
+		first, second = second, max(first+nums[i], second)
+	}
+	return second
+}
+
 func max(x, y int) int {
 	if x > y {
 		return x
