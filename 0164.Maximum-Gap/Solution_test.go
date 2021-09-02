d7862f2a9669dc3c0a84c95cf90e86304e9cda83
duanbiaowu <codean.net@gmail.com>
2021-09-02 23:06:58 +0800

    [feat] 0164.Maximum-Gap solution

---
+++ 0164.Maximum-Gap/Solution_test.go
@@ -0,0 +1,37 @@
+package leetcode
+
+import "testing"
+
+func Test_maximumGap(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"test-1",
+			args{nil},
+			0,
+		},
+		{
+			"test-2",
+			args{[]int{1}},
+			0,
+		},
+		{
+			"test-3",
+			args{[]int{3, 6, 9, 1}},
+			3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maximumGap(tt.args.nums); got != tt.want {
+				t.Errorf("maximumGap() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
