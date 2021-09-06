3ae30ec0f7132ad6eb5282c6d66ca44ef9a7d93b
duanbiaowu <codean.net@gmail.com>
2021-09-07 07:59:26 +0800

    [feat] 0143.Reorder-List sample solution

--- 0143.Reorder-List/Solution.go
+++ 0143.Reorder-List/Solution.go
@@ -25,3 +25,53 @@ func reorderList(head *ListNode) {
 	}
 	nodes[left].Next = nil
 }
+
+func middleNode(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return slow
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+
+	for cur := head; cur != nil; {
+		next := cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = next
+	}
+
+	return prev
+}
+
+func mergeList(head1, head2 *ListNode) {
+	var tmp1, tmp2 *ListNode
+	for head1 != nil && head2 != nil {
+		tmp1 = head1.Next
+		tmp2 = head2.Next
+
+		head1.Next = head2
+		head1 = tmp1
+
+		head2.Next = tmp1
+		head2 = tmp2
+	}
+}
+
+func reorderList2(head *ListNode) {
+	if head == nil {
+		return
+	}
+
+	mid := middleNode(head)
+	head1 := head
+	head2 := mid.Next
+	mid.Next = nil
+
+	head2 = reverseList(head2)
+	mergeList(head1, head2)
+}
