bd2ba9093cfb640086cc6483c669ec3cbab12520
duanbiaowu <codean.net@gmail.com>
2021-09-10 08:03:14 +0800

    [feat] GenerateRandomListNodesBySlice

--- structures/ListNode.go
+++ structures/ListNode.go
@@ -5,6 +5,12 @@ type ListNode struct {
 	Next *ListNode
 }
 
+type RandomListNode struct {
+	Val    int
+	Next   *RandomListNode
+	Random *RandomListNode
+}
+
 func GenerateListNodesByArray(nums []int) *ListNode {
 	dummy := &ListNode{}
 	cur := dummy
@@ -12,6 +18,27 @@ func GenerateListNodesByArray(nums []int) *ListNode {
 	for _, val := range nums {
 		cur.Next = &ListNode{Val: val}
 		cur = cur.Next
+	}
+
+	return dummy.Next
+}
+
+func GenerateRandomListNodesBySlice(nums [][2]int) *RandomListNode {
+	dummy := &RandomListNode{}
+	cur := dummy
+	m := map[int]*RandomListNode{}
+
+	for _, v := range nums {
+		node := &RandomListNode{Val: v[0]}
+		m[node.Val] = node
+		cur.Next = node
+		cur = cur.Next
+	}
+
+	for _, v := range nums {
+		if node, ok := m[v[1]]; ok {
+			m[v[0]].Random = node
+		}
 	}
 
 	return dummy.Next
--- structures/ListNode_test.go
+++ structures/ListNode_test.go
@@ -47,3 +47,77 @@ func TestGenerateListNodesByArray(t *testing.T) {
 		})
 	}
 }
+
+func TestGenerateRandomListNodesBySlice(t *testing.T) {
+	type args struct {
+		nums [][2]int
+	}
+
+	header := &RandomListNode{Val: 1}
+	node2 := &RandomListNode{Val: 2}
+	node3 := &RandomListNode{Val: 3}
+	node4 := &RandomListNode{Val: 4}
+	node5 := &RandomListNode{Val: 5}
+
+	node4.Next = node5
+	node3.Next = node4
+	node2.Next = node3
+	header.Next = node2
+
+	node3.Random = header
+	node4.Random = node2
+
+	tests := []struct {
+		name string
+		args args
+		want *RandomListNode
+	}{
+		{
+			"test an empty slice",
+			args{nil},
+			nil,
+		},
+		{
+			"test an slice with only one element",
+			args{[][2]int{
+				{1, 0},
+			}},
+			&RandomListNode{Val: 1},
+		},
+		{
+			"test an slice of multiple elements with no random elements",
+			args{[][2]int{
+				{1, 0},
+				{2, 0},
+				{3, 0},
+			}},
+			&RandomListNode{
+				Val: 1,
+				Next: &RandomListNode{
+					Val: 2,
+					Next: &RandomListNode{
+						Val: 3,
+					},
+				},
+			},
+		},
+		{
+			"test an slice of multiple elements with random elements",
+			args{[][2]int{
+				{1, 0},
+				{2, 0},
+				{3, 1},
+				{4, 2},
+				{5, 0},
+			}},
+			header,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GenerateRandomListNodesBySlice(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GenerateRandomListNodesBySlice() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
