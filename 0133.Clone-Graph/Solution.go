1a23e182e68e43cbbb2edc39688d460909955cdc
duanbiaowu <codean.net@gmail.com>
2021-09-11 11:36:14 +0800

    [feat] 0133.Clone-Graph bfs solution

--- 0133.Clone-Graph/Solution.go
+++ 0133.Clone-Graph/Solution.go
@@ -26,3 +26,28 @@ func cloneGraphDFS(node *Node, visited map[*Node]*Node) *Node {
 	}
 	return cloneNode
 }
+
+func cloneGraphBFS(node *Node) *Node {
+	if node == nil {
+		return node
+	}
+
+	visited := make(map[*Node]*Node)
+	visited[node] = &Node{node.Val, []*Node{}}
+	queue := []*Node{node}
+
+	for len(queue) > 0 {
+		top := queue[0]
+		queue = queue[1:]
+
+		for _, v := range top.Neighbors {
+			if _, ok := visited[v]; !ok {
+				visited[v] = &Node{v.Val, []*Node{}}
+				queue = append(queue, v)
+			}
+			visited[top].Neighbors = append(visited[top].Neighbors, visited[v])
+		}
+	}
+
+	return visited[node]
+}
