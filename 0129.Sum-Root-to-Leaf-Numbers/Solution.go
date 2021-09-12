20ae37673dfeb61110df2e1f0bf4e0f0f04df1db
duanbiaowu <codean.net@gmail.com>
2021-09-12 11:24:28 +0800

    [feat] 0129.Sum-Root-to-Leaf-Numbers bfs solution

--- 0129.Sum-Root-to-Leaf-Numbers/Solution.go
+++ 0129.Sum-Root-to-Leaf-Numbers/Solution.go
@@ -18,3 +18,35 @@ func dfs(root *TreeNode, prevSum int) int {
 	}
 	return dfs(root.Left, sum) + dfs(root.Right, sum)
 }
+
+func bfs(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	sum := 0
+	nodeQueue := []*TreeNode{root}
+	numQueue := []int{root.Val}
+
+	for len(nodeQueue) > 0 {
+		node := nodeQueue[0]
+		nodeQueue = nodeQueue[1:]
+		num := numQueue[0]
+		numQueue = numQueue[1:]
+
+		if node.Left == nil && node.Right == nil {
+			sum += num
+		} else {
+			if node.Left != nil {
+				nodeQueue = append(nodeQueue, node.Left)
+				numQueue = append(numQueue, num*10+node.Left.Val)
+			}
+			if node.Right != nil {
+				nodeQueue = append(nodeQueue, node.Right)
+				numQueue = append(numQueue, num*10+node.Right.Val)
+			}
+		}
+	}
+
+	return sum
+}
