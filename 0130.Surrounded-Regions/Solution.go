7a37a1ab4cad32348cec8f2d08b6496b28699bb8
duanbiaowu <codean.net@gmail.com>
2021-09-12 10:51:58 +0800

    [feat] 0130.Surrounded-Regions bfs solution

--- 0130.Surrounded-Regions/Solution.go
+++ 0130.Surrounded-Regions/Solution.go
@@ -7,6 +7,7 @@ func solve(board [][]byte) {
 
 	// 题目解释中提到：任何边界上的 O 都不会被填充为 X
 	// 所有的不被包围的 O 都直接或间接与边界上的 O 相连
+	// 将所有可以连通的进行标记，剩下的就是无法连通的
 	n, m := len(board), len(board[0])
 
 	// 左右边界
@@ -42,4 +43,62 @@ func dfs(board [][]byte, x, y int) {
 	dfs(board, x-1, y)
 	dfs(board, x, y+1)
 	dfs(board, x, y-1)
+}
+
+func bfs(board [][]byte) {
+	if len(board) == 0 || len(board[0]) == 0 {
+		return
+	}
+
+	n, m := len(board), len(board[0])
+	var queue [][2]int
+
+	// 左右边界
+	for i := 0; i < n; i++ {
+		if board[i][0] == 'O' {
+			queue = append(queue, [2]int{i, 0})
+			board[i][0] = '#'
+		}
+		if board[i][m-1] == 'O' {
+			queue = append(queue, [2]int{i, m - 1})
+			board[i][m-1] = '#'
+		}
+	}
+
+	// 上下边界
+	for i := 1; i < m-1; i++ {
+		if board[0][i] == 'O' {
+			queue = append(queue, [2]int{0, i})
+			board[0][i] = '#'
+		}
+		if board[n-1][i] == 'O' {
+			queue = append(queue, [2]int{n - 1, i})
+			board[n-1][i] = '#'
+		}
+	}
+
+	dx := []int{0, 1, 0, -1} // x: 上右下左
+	dy := []int{-1, 0, 1, 0} // y: 上右下左
+
+	for len(queue) > 0 {
+		top := queue[0]
+		queue = queue[1:]
+		for i := 0; i < 4; i++ {
+			x, y := top[0]+dx[i], top[1]+dy[i]
+			if x > 0 && x < n && y > 0 && y < m && board[x][y] == 'O' {
+				queue = append(queue, [2]int{x, y})
+				board[x][y] = '#'
+			}
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if board[i][j] == '#' {
+				board[i][j] = 'O'
+			} else if board[i][j] == 'O' {
+				board[i][j] = 'X'
+			}
+		}
+	}
 }
