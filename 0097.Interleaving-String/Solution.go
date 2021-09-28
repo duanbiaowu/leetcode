866a300fa853f74b792d684dec2d85c8dac8459f
duanbiaowu <codean.net@gmail.com>
2021-09-29 07:46:41 +0800

    [feat] 0097.Interleaving-String solution

--- 0097.Interleaving-String/Solution.go
+++ 0097.Interleaving-String/Solution.go
@@ -1,5 +1,6 @@
 package leetcode
 
+// https://leetcode-cn.com/problems/interleaving-string/solution/jiao-cuo-zi-fu-chuan-by-leetcode-solution/
 func isInterleave(s1 string, s2 string, s3 string) bool {
 	n, m, t := len(s1), len(s2), len(s3)
 	if n == 0 && m == 0 && t == 0 {
@@ -28,4 +29,30 @@ func isInterleave(s1 string, s2 string, s3 string) bool {
 	}
 
 	return dp[n][m]
+}
+
+func isInterleave2(s1 string, s2 string, s3 string) bool {
+	n, m, t := len(s1), len(s2), len(s3)
+	if n == 0 && m == 0 && t == 0 {
+		return true
+	}
+	if n+m != t {
+		return false
+	}
+
+	dp := make([]bool, m+1)
+	dp[0] = true
+	for i := 0; i <= n; i++ {
+		for j := 0; j <= m; j++ {
+			p := i + j - 1
+			if i > 0 {
+				dp[j] = dp[j] && s1[i-1] == s3[p]
+			}
+			if j > 0 {
+				dp[j] = dp[j] || (dp[j-1] && s2[j-1] == s3[p])
+			}
+		}
+	}
+
+	return dp[m]
 }
