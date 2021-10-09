b0e7fdd7223cf8e3623c92ad20f6652cf7f23f8c
duanbiaowu <codean.net@gmail.com>
2021-10-09 22:44:40 +0800

    [feat] 0065.Valid-Number simple solution

---
+++ 0065.Valid-Number/Solution_test.go
@@ -0,0 +1,47 @@
+package leetcode
+
+import "testing"
+
+func Test_isNumber(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"test-1",
+			args{""},
+			false,
+		},
+		{
+			"test-2",
+			args{"0"},
+			true,
+		},
+		{
+			"test-3",
+			args{"e"},
+			false,
+		},
+		{
+			"test-4",
+			args{"."},
+			false,
+		},
+		{
+			"test-5",
+			args{".1"},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isNumber(tt.args.s); got != tt.want {
+				t.Errorf("isNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
