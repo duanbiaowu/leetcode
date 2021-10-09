27e2819b0a256d369f533f972d6b1e5abe820905
duanbiaowu <codean.net@gmail.com>
2021-10-09 23:07:33 +0800

    [feat] 0065.Valid-Number DFA solution

--- 0065.Valid-Number/Solution.go
+++ 0065.Valid-Number/Solution.go
@@ -18,3 +18,98 @@ func isNumber(s string) bool {
 	}
 	return numFlag
 }
+
+type State int
+type CharType int
+
+const (
+	StateInitial State = iota
+	StateIntSign
+	StateInteger
+	StatePoint
+	StatePointWithoutInt
+	StateFraction
+	StateEx
+	StateExpSign
+	StateExpNumber
+	StateEnd
+)
+
+const (
+	CharNumber CharType = iota
+	CharExp
+	CharPoint
+	CharSign
+	CharIllegal
+)
+
+func toCharType(char byte) CharType {
+	switch char {
+	case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+		return CharNumber
+	case 'e', 'E':
+		return CharExp
+	case '.':
+		return CharPoint
+	case '+', '-':
+		return CharSign
+	default:
+		return CharIllegal
+	}
+}
+
+// DFA
+// https://leetcode-cn.com/problems/valid-number/solution/you-xiao-shu-zi-by-leetcode-solution-298l/
+func isNumber2(s string) bool {
+	transfer := map[State]map[CharType]State{
+		StateInitial: {
+			CharNumber: StateInteger,
+			CharPoint:  StatePointWithoutInt,
+			CharSign:   StateIntSign,
+		},
+		StateIntSign: {
+			CharNumber: StateInteger,
+			CharPoint:  StatePointWithoutInt,
+		},
+		StateInteger: {
+			CharNumber: StateInteger,
+			CharExp:    StateEx,
+			CharPoint:  StatePoint,
+		},
+		StatePoint: {
+			CharNumber: StateFraction,
+			CharExp:    StateEx,
+		},
+		StatePointWithoutInt: {
+			CharNumber: StateFraction,
+		},
+		StateFraction: {
+			CharNumber: StateFraction,
+			CharExp:    StateEx,
+		},
+		StateEx: {
+			CharNumber: StateExpNumber,
+			CharSign:   StateExpSign,
+		},
+		StateExpSign: {
+			CharNumber: StateExpNumber,
+		},
+		StateExpNumber: {
+			CharNumber: StateExpNumber,
+		},
+	}
+
+	state := StateInitial
+	for i := 0; i < len(s); i++ {
+		charType := toCharType(s[i])
+		if _, ok := transfer[state][charType]; !ok {
+			return false
+		} else {
+			state = transfer[state][charType]
+		}
+	}
+
+	return state == StateInteger || state == StatePoint ||
+		state == StateFraction || state == StateExpNumber ||
+		state == StateEnd
+}
