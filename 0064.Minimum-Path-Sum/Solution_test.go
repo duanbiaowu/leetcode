2958d0be874a50b98a0715085977662fc694e960
duanbiaowu <codean.net@gmail.com>
2021-10-10 21:23:23 +0800

    [feat] 0064.Minimum-Path-Sum solution

---
+++ 0064.Minimum-Path-Sum/Solution_test.go
@@ -0,0 +1,36 @@
+package leetcode
+
+import "testing"
+
+func Test_minPathSum(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"test-1",
+			args{nil},
+			0,
+		},
+		{
+			"test-2",
+			args{[][]int{
+				{1, 3, 1},
+				{1, 5, 1},
+				{4, 2, 1},
+			}},
+			7,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minPathSum(tt.args.grid); got != tt.want {
+				t.Errorf("minPathSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
