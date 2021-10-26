6e1761f068ce595d9d015a0f8f4d3f7ebe708c60
duanbiaowu <codean.net@gmail.com>
2021-10-26 12:15:25 +0800

    [refactor] 0039.Combination-Sum solution

--- 0039.Combination-Sum/Solution.go
+++ 0039.Combination-Sum/Solution.go
@@ -10,19 +10,19 @@ func combinationSum(candidates []int, target int) [][]int {
 	var path []int
 	var res [][]int
 	sort.Ints(candidates)
-	backtrack(candidates, target, 0, path, &res)
+	backtrack(candidates, target, 0, &path, &res)
 	return res
 }
 
-func backtrack(nums []int, target, begin int, path []int, res *[][]int) {
+func backtrack(nums []int, target, begin int, path *[]int, res *[][]int) {
 	if target == 0 {
-		*res = append(*res, append([]int{}, path...))
+		*res = append(*res, append([]int{}, *path...))
 		return
 	}
 
 	for i := begin; i < len(nums) && nums[i] <= target; i++ {
-		path = append(path, nums[i])
+		*path = append(*path, nums[i])
 		backtrack(nums, target-nums[i], i, path, res)
-		path = path[:len(path)-1]
+		*path = (*path)[:len(*path)-1]
 	}
 }
