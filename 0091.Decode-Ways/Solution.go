4bf90a0643d0beb448296f3b2adc05f808b697fc
duanbiaowu <codean.net@gmail.com>
2021-10-27 15:21:08 +0800

    [refactor] 0091.Decode-Ways solution

--- 0091.Decode-Ways/Solution.go
+++ 0091.Decode-Ways/Solution.go
@@ -2,6 +2,23 @@ package leetcode
 
 func numDecodings(s string) int {
 	n := len(s)
+	// x = f[i-2], y = f[i-1], z = f[i]
+	x, y, z := 0, 1, 0
+	for i := 1; i <= n; i++ {
+		z = 0
+		if s[i-1] != '0' {
+			z += y
+		}
+		if i > 1 && s[i-2] != '0' && ((s[i-1]-'0')+(s[i-2]-'0')*10 <= 26) {
+			z += x
+		}
+		x, y = y, z
+	}
+	return z
+}
+
+func numDecodings2(s string) int {
+	n := len(s)
 	if n == 0 {
 		return 0
 	}
