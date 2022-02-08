366d294f16fdac9529b7955c2c2475dbdbc88f39
duanbiaowu <codean.net@gmail.com>
2022-02-08 22:17:39 +0800

    feat: Interview/0208-Detect-Cycle/ solution

---
+++ Interview/0208-Detect-Cycle/Solution_test.go
@@ -0,0 +1,53 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_detectCycle(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+
+	cycleListNode := &ListNode{Val: 1}
+	cycleListNode.Next = &ListNode{
+		Val: 2,
+		Next: &ListNode{
+			Val: 3,
+			Next: &ListNode{
+				Val:  4,
+				Next: cycleListNode,
+			},
+		},
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			"test-1",
+			args{nil},
+			nil,
+		},
+		{
+			"test-2",
+			args{&ListNode{Val: 1}},
+			nil,
+		},
+		{
+			"test-3",
+			args{cycleListNode},
+			cycleListNode,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := detectCycle(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("detectCycle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
