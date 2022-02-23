df60a04e7e2e65ce60bc64a4a552772b694e325e
duanbiaowu <codean.net@gmail.com>
2022-02-23 23:05:50 +0800

    feat: 0050.Powx-n solution

--- 0050.Powx-n/Solution.go
+++ 0050.Powx-n/Solution.go
@@ -18,3 +18,21 @@ func quickMul(x float64, n int) float64 {
 	}
 	return y * y * x
 }
+
+func myPow2(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	}
+	if n == 1 {
+		return x
+	}
+	if n == -1 {
+		return 1 / x
+	}
+	// 注意这里不能写为: n>>1
+	// -3>>1 = -2
+	// -3/2 = -1
+	half := myPow2(x, n/2)
+	mod := myPow2(x, n%2)
+	return half * half * mod
+}
