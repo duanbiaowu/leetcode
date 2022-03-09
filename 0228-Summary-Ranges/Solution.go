67dd1e67e41f9fa84666c0c285170ebeb2650af9
duanbiaowu <codean.net@gmail.com>
2022-03-09 08:53:52 +0800

    feat: 0228-Summary-Ranges solution

---
+++ 0228-Summary-Ranges/Solution.go
@@ -0,0 +1,20 @@
+package leetcode
+
+import "strconv"
+
+func summaryRanges(nums []int) []string {
+	var res []string
+	for right, n := 0, len(nums); right < n; {
+		left := right
+		right++
+		for right < n && nums[right] == nums[right-1]+1 {
+			right++
+		}
+		s := strconv.Itoa(nums[left])
+		if left < right-1 {
+			s += "->" + strconv.Itoa(nums[right-1])
+		}
+		res = append(res, s)
+	}
+	return res
+}
---
+++ 0228-Summary-Ranges/Solution_test.go
@@ -0,0 +1,35 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_summaryRanges(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			"test-1",
+			args{nil},
+			nil,
+		},
+		{
+			"test-2",
+			args{[]int{0, 1, 2, 4, 5, 7}},
+			[]string{"0->2", "4->5", "7"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := summaryRanges(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("summaryRanges() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
