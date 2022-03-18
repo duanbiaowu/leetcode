99df9ed7ed1ca8ab14f4eccd1a09cc163484c2ef
duanbiaowu <codean.net@gmail.com>
2022-03-18 09:13:10 +0800

    refactor: 0011.Container-With-Most-Water solution

--- 0011.Container-With-Most-Water/Solution.go
+++ 0011.Container-With-Most-Water/Solution.go
@@ -1,22 +1,24 @@
 package leetcode
 
+// 如果直接看图，思路容易错误的地方在于：Y 轴坐标宽度占用宽度
+// 其实题目要求只求出一个相对最大区域即可
 func maxArea(height []int) int {
 	left, right := 0, len(height)-1
 	res := 0
 
 	for left < right {
 		width := right - left
-		moreHeight := 0
+		short := 0
 		if height[left] < height[right] {
-			moreHeight = height[left]
+			short = height[left]
 			left++
 		} else {
-			moreHeight = height[right]
+			short = height[right]
 			right--
 		}
-		res = max(res, width*moreHeight)
+		res = max(res, width*short)
 	}
-	
+
 	return res
 }
 
