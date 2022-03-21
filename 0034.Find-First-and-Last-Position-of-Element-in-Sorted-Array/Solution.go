7fafa1b49b968d842ce05336957c0ba0d8a6e560
duanbiaowu <codean.net@gmail.com>
2022-03-21 10:39:15 +0800

    feat: 0034.Find-First-and-Last-Position-of-Element-in-Sorted-Array solution

--- 0034.Find-First-and-Last-Position-of-Element-in-Sorted-Array/Solution.go
+++ 0034.Find-First-and-Last-Position-of-Element-in-Sorted-Array/Solution.go
@@ -21,3 +21,41 @@ func search(nums []int, target int) int {
 	}
 	return low
 }
+
+func searchRange2(nums []int, target int) []int {
+	res := []int{-1, -1}
+	n := len(nums)
+	low, hi := 0, n-1
+	if hi < 0 {
+		return res
+	}
+
+	// 寻找左边界，由右侧逼近
+	for low <= hi {
+		mid := low + (hi-low)>>1
+		if nums[mid] >= target {
+			hi = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+
+	if low >= n || nums[low] != target {
+		return res
+	}
+
+	res[0] = low
+	// 寻找右边界，由左侧逼近
+	hi = n - 1
+	for low <= hi {
+		mid := low + (hi-low)>>1
+		if nums[mid] <= target {
+			low = mid + 1
+		} else {
+			hi = mid - 1
+		}
+	}
+	res[1] = low - 1
+
+	return res
+}
--- 0034.Find-First-and-Last-Position-of-Element-in-Sorted-Array/Solution_test.go
+++ 0034.Find-First-and-Last-Position-of-Element-in-Sorted-Array/Solution_test.go
@@ -25,6 +25,21 @@ func Test_searchRange(t *testing.T) {
 			args{[]int{5, 7, 7, 8, 8, 10}, 8},
 			[]int{3, 4},
 		},
+		{
+			"test-3",
+			args{[]int{2, 2}, 3},
+			[]int{-1, -1},
+		},
+		{
+			"test-4",
+			args{[]int{2, 2}, 2},
+			[]int{0, 1},
+		},
+		{
+			"test-5",
+			args{[]int{1}, 1},
+			[]int{0, 0},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
