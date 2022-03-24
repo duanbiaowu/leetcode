448d7f993e655bce79315d7e26c5949764012db7
duanbiaowu <codean.net@gmail.com>
2022-03-24 08:15:35 +0800

    feat: 0062.Unique-Paths solution

--- 0062.Unique-Paths/Solution.go
+++ 0062.Unique-Paths/Solution.go
@@ -5,12 +5,15 @@ func uniquePaths(m int, n int) int {
 		return 0
 	}
 
+	// dp[i][j] -> 从 (0,0) 到 (i,j) 的路径数目
 	dp := make([][]int, m)
 	for i := 0; i < m; i++ {
 		dp[i] = make([]int, n)
+		// 不能往左走
 		dp[i][0] = 1
 	}
 	for j := 0; j < n; j++ {
+		// 不能往上走
 		dp[0][j] = 1
 	}
 	for i := 1; i < m; i++ {
@@ -19,4 +22,22 @@ func uniquePaths(m int, n int) int {
 		}
 	}
 	return dp[m-1][n-1]
+}
+
+func uniquePaths2(m int, n int) int {
+	if m <= 0 || n <= 0 {
+		return 0
+	}
+
+	// 空间优化：每次只需要 dp[i-1][j], dp[i][j-1]
+	dp := make([]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = 1
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[j] += dp[j-1]
+		}
+	}
+	return dp[n-1]
 }
