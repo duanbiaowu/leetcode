71628e3d619caa89a0267c6f55977e5ee419ca5a
duanbiaowu <codean.net@gmail.com>
2022-03-29 21:26:57 +0800

    docs: 0160.Intersection-of-Two-Linked-Lists solution

--- 0160.Intersection-of-Two-Linked-Lists/Solution.go
+++ 0160.Intersection-of-Two-Linked-Lists/Solution.go
@@ -2,13 +2,13 @@ package leetcode
 
 import "github.com/duanbiaowu/leetcode/structures"
 
+// ListNode Definition for singly-linked list.
 type ListNode = structures.ListNode
 
+// 设交集链表长 c, 链表 1 除交集的长度为 a，链表 2 除交集的长度为 b:
+// a + c + b = b + c + a
+// 若无交集，则 a + b = b + a, 返回 null
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
-	if headA == nil || headB == nil {
-		return nil
-	}
-
 	// 反向指向: 任一链表到尾节点时, 指向对方链表头节点
 	// 消除链接长度差
 	pa, pb := headA, headB
