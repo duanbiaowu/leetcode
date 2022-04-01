9c59709c181e6b749fe1aded037363f16c4e7aed
duanbiaowu <codean.net@gmail.com>
2022-04-01 18:40:25 +0800

    feat: 0287-Find-The-Duplicate-Number solution

--- 0287-Find-The-Duplicate-Number/Solution.go
+++ 0287-Find-The-Duplicate-Number/Solution.go
@@ -19,3 +19,23 @@ func findDuplicate(nums []int) int {
 	}
 	return low
 }
+
+func findDuplicate2(nums []int) int {
+	// 数字都在 1 到 n 之间（包括 1 和 n）
+	// 由于存在的重复的数字 target，因此 target 这个位置一定有起码两条指向它的边
+	// 因此整张图一定存在环，且我们要找到的 target 就是这个环的入口
+	slow, fast := nums[0], nums[nums[0]]
+	// 直到快慢指针相遇
+	for fast != slow {
+		slow = nums[slow]
+		fast = nums[nums[fast]]
+	}
+
+	// 两个指针每次同时移动一步，相遇的点就是 target
+	slow = 0
+	for slow != fast {
+		slow = nums[slow]
+		fast = nums[fast]
+	}
+	return slow
+}
