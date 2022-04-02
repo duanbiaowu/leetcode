72b69644947da6dcda612083f6abf779e7f8bcc8
duanbiaowu <codean.net@gmail.com>
2022-04-03 07:36:10 +0800

    docs: 0581-Find-Unsorted-Subarray solution

--- 0581-Find-Unsorted-Subarray/Solution.go
+++ 0581-Find-Unsorted-Subarray/Solution.go
@@ -1,6 +1,10 @@
 package leetcode
 
 // reference: https://leetcode-cn.com/problems/shortest-unsorted-continuous-subarray/solution/si-lu-qing-xi-ming-liao-kan-bu-dong-bu-cun-zai-de-/
+// 1. 把数组分为 3 段
+// 		左段和右段是无序的，
+//		但是满足最小值大于 左段 的最大值，最大值小于 右段 的最小值
+// 2. 寻找中段的左右边界
 func findUnsortedSubarray(nums []int) int {
 	n := len(nums)
 	if n < 2 {
@@ -9,7 +13,7 @@ func findUnsortedSubarray(nums []int) int {
 
 	max, min := nums[0], nums[n-1]
 	left, right := 0, -1
-	for i := 0; i < n; i++ {
+	for i := 1; i < n; i++ {
 		// 从左到右维持最大值，寻找右边界
 		if nums[i] < max {
 			right = i
@@ -24,6 +28,5 @@ func findUnsortedSubarray(nums []int) int {
 			min = nums[n-i-1]
 		}
 	}
-
 	return right - left + 1
 }
