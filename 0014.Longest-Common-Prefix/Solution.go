07fd56a21a4be76078459e35df58282164d7e35d
duanbiaowu <codean.net@gmail.com>
2024-03-02 14:30:42 +0800

    docs: archive

--- 0014.Longest-Common-Prefix/Solution.go
+++ 0014.Longest-Common-Prefix/Solution.go
@@ -5,6 +5,8 @@ func longestCommonPrefix(strs []string) string {
 		return ""
 	}
 
+	// 直接以第一个数组元素字符串为基准进行对比即可
+	// 当然这里可以使用任意的数组元素作为基准字符串
 	for i := 0; i < len(strs[0]); i++ {
 		for j := 1; j < len(strs); j++ {
 			if i == len(strs[j]) || strs[j][i] != strs[0][i] {
@@ -12,5 +14,6 @@ func longestCommonPrefix(strs []string) string {
 			}
 		}
 	}
+	// 代码执行到这里，说明第一个数组元素字符串本身就是最长前缀
 	return strs[0]
 }
