db4e2ee2767cda3b7fefd92066c17a79f54142e8
duanbiaowu <codean.net@gmail.com>
2024-03-10 10:15:46 +0800

    docs: archive

--- 0290-Word-Pattern/Solution.go
+++ 0290-Word-Pattern/Solution.go
@@ -3,22 +3,27 @@ package leetcode
 import "strings"
 
 func wordPattern(pattern string, s string) bool {
-	// 双射
-	word2Ch := map[string]byte{}
-	ch2Word := map[byte]string{}
+	// 双射解题
+	wordToChar := make(map[string]byte)
+	charToWord := make(map[byte]string)
+
 	words := strings.Split(s, " ")
 	if len(pattern) != len(words) {
 		return false
 	}
 
 	for i, word := range words {
-		ch := pattern[i]
-		if word2Ch[word] > 0 && word2Ch[word] != ch ||
-			ch2Word[ch] != "" && ch2Word[ch] != word {
+		char := pattern[i]
+		// 如果不满足双射条件，直接返回
+		if wordToChar[word] > 0 && wordToChar[word] != char {
 			return false
 		}
-		word2Ch[word] = ch
-		ch2Word[ch] = word
+		if len(charToWord[char]) > 0 && charToWord[char] != word {
+			return false
+		}
+
+		wordToChar[word] = char
+		charToWord[char] = word
 	}
 
 	return true
