ae4c0e664850f5bd8df8789bf1d87f32d9060aad
duanbiaowu <codean.net@gmail.com>
2024-03-13 12:17:31 +0800

    docs: archive

--- 0098.Validate-Binary-Search-Tree/Solution.go
+++ 0098.Validate-Binary-Search-Tree/Solution.go
@@ -25,21 +25,51 @@ func helper(root *TreeNode, low, hi int) bool {
 	return helper(root.Left, low, root.Val) && helper(root.Right, root.Val, hi)
 }
 
-func isValidBSTInOrder(root *TreeNode) bool {
+func isValidBST2(root *TreeNode) bool {
+	inOrder := math.MinInt64
+
 	var stack []*TreeNode
-	inorder := math.MinInt64
 	for len(stack) > 0 || root != nil {
 		for root != nil {
 			stack = append(stack, root)
 			root = root.Left
 		}
+
 		root = stack[len(stack)-1]
 		stack = stack[:len(stack)-1]
-		if root.Val <= inorder {
+		if root.Val <= inOrder {
 			return false
 		}
-		inorder = root.Val
+
+		inOrder = root.Val
 		root = root.Right
 	}
+
 	return true
+}
+
+// 二叉搜索树中序遍历满足有序性
+// 通过中序遍历将值保存到数组中
+// 然后判断是否有序即可
+func isValidBSTInOrder(root *TreeNode) bool {
+	var res []int
+	inOrder(root, &res)
+
+	for i := 0; i < len(res)-1; i++ {
+		if res[i] >= res[i+1] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func inOrder(root *TreeNode, res *[]int) {
+	if root == nil {
+		return
+	}
+
+	inOrder(root.Left, res)
+	*res = append(*res, root.Val)
+	inOrder(root.Right, res)
 }
