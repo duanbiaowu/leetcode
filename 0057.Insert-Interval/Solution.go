a1d7ed181bc0ed3f7b7fcc9b806527768c0b2e11
duanbiaowu <codean.net@gmail.com>
2024-03-16 08:56:03 +0800

    docs: archive

--- 0057.Insert-Interval/Solution.go
+++ 0057.Insert-Interval/Solution.go
@@ -1,33 +1,40 @@
 package leetcode
 
 func insert(intervals [][]int, newInterval []int) [][]int {
+	// 边界情况处理
 	if newInterval == nil {
 		return intervals
 	}
 	if intervals == nil {
-		return append(make([][]int, 0), newInterval)
+		return [][]int{newInterval}
 	}
 
 	var res [][]int
 	left, right := newInterval[0], newInterval[1]
+	// 区间是否已合并标记
 	merged := false
-	for i := 0; i < len(intervals); i++ {
-		if intervals[i][0] > right {
-			// 在当前区间右侧并且无交集
+
+	for _, val := range intervals {
+		if val[0] > right {
+			// 新区间和当前区间右边界无重叠
 			if !merged {
+				// 更新区间是否已合并标记
 				merged = true
 				res = append(res, []int{left, right})
 			}
-			res = append(res, intervals[i])
-		} else if intervals[i][1] < left {
-			// 在当前区间左侧并且无交集
-			res = append(res, intervals[i])
+			res = append(res, val)
+		} else if val[1] < left {
+			// 新区间和当前区间左边界无重叠
+			res = append(res, val)
 		} else {
-			// 与当前区间有交集，计算并集
-			left = min(left, intervals[i][0])
-			right = max(right, intervals[i][1])
+			// 新区间和当前区间重叠
+			left = min(left, val[0])
+			right = max(right, val[1])
 		}
 	}
+
+	// 代码执行到这里还没有合并
+	// 说明新区间比当前所有区间都要大
 	if !merged {
 		res = append(res, []int{left, right})
 	}
