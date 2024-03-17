bddef1a1b7db6595122f7b567d52f9904112275f
duanbiaowu <codean.net@gmail.com>
2024-03-18 07:57:42 +0800

    docs: archive

--- 0153.Find-Minimum-in-Rotated-Sorted-Array/Solution.go
+++ 0153.Find-Minimum-in-Rotated-Sorted-Array/Solution.go
@@ -2,13 +2,24 @@ package leetcode
 
 func findMin(nums []int) int {
 	low, hi := 0, len(nums)-1
+
 	for low < hi {
 		mid := low + (hi-low)>>1
 		if nums[mid] < nums[hi] {
+			// 为什么 high = mid, 而不是 high = mid-1 ?
+			// 因为 nums[mid] 有可能就是最小值
+			// 示例: {4, 5, 1, 2, 3}
+			// 如果 high = mid - 1，则丢失了最小值 1
 			hi = mid
 		} else {
+			// 为什么 low = mid + 1, 而不是 low = mid ?
+			// 因为 nums[mid] 不可能是最小值
+			// 因为如果 nums[mid] 是最小值
+			// 那么其会满足上面的表达式: nums[mid] < nums[hi]
+			// 代码就不会执行到这里了
 			low = mid + 1
 		}
 	}
+
 	return nums[low]
 }
