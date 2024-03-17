b54954c7767c08ce3940f8b21959a36bd4c1cd4f
duanbiaowu <codean.net@gmail.com>
2024-03-17 10:37:50 +0800

    docs: archive

--- 0074.Search-a-2D-Matrix/Solution.go
+++ 0074.Search-a-2D-Matrix/Solution.go
@@ -3,16 +3,18 @@ package leetcode
 // 将矩阵每一行拼接在上一行的末尾，则会得到一个升序数组
 // 可以在该数组上二分查找
 func searchMatrix(matrix [][]int, target int) bool {
-	m := len(matrix)
-	if m == 0 {
+	rows := len(matrix)
+	if rows == 0 {
 		return false
 	}
 
-	n := len(matrix[0])
-	low, hi := 0, m*n-1
+	cols := len(matrix[0])
+	low, hi := 0, rows*cols-1
 	for low <= hi {
 		mid := low + (hi-low)>>1
-		val := matrix[mid/n][mid%n]
+		// 将索引转换为具体的矩阵坐标
+		val := matrix[mid/cols][mid%cols]
+
 		if val == target {
 			return true
 		} else if val < target {
@@ -20,7 +22,6 @@ func searchMatrix(matrix [][]int, target int) bool {
 		} else {
 			hi = mid - 1
 		}
-
 	}
 
 	return false
