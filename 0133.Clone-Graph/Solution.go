7bc118e91aed6fab83fa6cce54b19a453a0e86df
duanbiaowu <codean.net@gmail.com>
2024-03-19 07:54:04 +0800

    docs: archive

--- 0133.Clone-Graph/Solution.go
+++ 0133.Clone-Graph/Solution.go
@@ -1,15 +1,16 @@
 package leetcode
 
+// Noe Definition for a Node.
 type Node struct {
 	Val       int
 	Neighbors []*Node
 }
 
 func cloneGraph(node *Node) *Node {
-	return cloneGraphDFS(node, make(map[*Node]*Node))
+	return dfs(node, make(map[*Node]*Node))
 }
 
-func cloneGraphDFS(node *Node, visited map[*Node]*Node) *Node {
+func dfs(node *Node, visited map[*Node]*Node) *Node {
 	if node == nil {
 		return node
 	}
@@ -18,16 +19,20 @@ func cloneGraphDFS(node *Node, visited map[*Node]*Node) *Node {
 		return visited[node]
 	}
 
+	// 克隆当前节点
 	cloneNode := &Node{node.Val, []*Node{}}
+	// 将当前节点标记为已经克隆
 	visited[node] = cloneNode
 
+	// 遍历当前的节点的邻节点，递归克隆
 	for _, v := range node.Neighbors {
-		cloneNode.Neighbors = append(cloneNode.Neighbors, cloneGraphDFS(v, visited))
+		cloneNode.Neighbors = append(cloneNode.Neighbors, dfs(v, visited))
 	}
+
 	return cloneNode
 }
 
-func cloneGraphBFS(node *Node) *Node {
+func bfs(node *Node) *Node {
 	if node == nil {
 		return node
 	}
