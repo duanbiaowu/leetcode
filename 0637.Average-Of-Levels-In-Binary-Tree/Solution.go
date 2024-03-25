28398e6b9c5f091c12cbe3b295e298d89d8b7144
duanbiaowu <codean.net@gmail.com>
2024-03-26 07:52:54 +0800

    docs: archive

--- 0637.Average-Of-Levels-In-Binary-Tree/Solution.go
+++ 0637.Average-Of-Levels-In-Binary-Tree/Solution.go
@@ -33,9 +33,15 @@ func averageOfLevels(root *TreeNode) []float64 {
 }
 
 func averageOfLevelsDFS(root *TreeNode) []float64 {
+	// sum 存储每层节点累加值
+	// cnt 存储每层节点数量
 	var sum, cnt []int
+
+	// 从 root 节点作为 DFS 起始节点
+	// 使用指针传递状态变量
 	dfs(root, 0, &sum, &cnt)
 
+	// 计算每层的平均值
 	var res []float64
 	for i := range sum {
 		res = append(res, float64(sum[i])/float64(cnt[i]))
@@ -44,23 +50,27 @@ func averageOfLevelsDFS(root *TreeNode) []float64 {
 	return res
 }
 
-func dfs(root *TreeNode, level int, sum, cnt *[]int) {
+func dfs(root *TreeNode, depth int, sum, cnt *[]int) {
+	// DFS 递归条件处理
 	if root == nil {
 		return
 	}
 
-	// 遇到各个层级的第一个节点时，初始化累加器和计数器
-	if level == len(*sum) {
-		// 树的每一层，这个分支只会执行一次
+	// 遇到每个层级的第一个节点时，初始化累加器和计数器
+	if depth == len(*sum) {
 		*sum = append(*sum, root.Val)
 		*cnt = append(*cnt, 1)
 	} else {
-		// 树的每一层，这个分支会执行 N - 1 次
+		// 遇到了每个层级的第 2 - N 个节点
 		// 其中 N 等于该层上面的节点数量
-		(*sum)[level] += root.Val
-		(*cnt)[level]++
+		// 对于树的每一层，这个分支会执行 N - 1 次
+		(*sum)[depth] += root.Val
+		(*cnt)[depth]++
 	}
 
-	dfs(root.Left, level+1, sum, cnt)
-	dfs(root.Right, level+1, sum, cnt)
+	// 递归左子树时，深度 + 1, 同时传递两个指针数组
+	dfs(root.Left, depth+1, sum, cnt)
+
+	// 递归右子树时，深度 + 1, 同时传递两个指针数组
+	dfs(root.Right, depth+1, sum, cnt)
 }
