a4a410f2df65cc77dd75da6bd3fc440ec9b26dd4
duanbiaowu <codean.net@gmail.com>
2024-03-27 17:40:43 +0800

    docs: archive

--- 0133.Clone-Graph/Solution.go
+++ 0133.Clone-Graph/Solution.go
@@ -33,26 +33,42 @@ func dfs(node *Node, visited map[*Node]*Node) *Node {
 }
 
 func bfs(node *Node) *Node {
+	// 边界处理
 	if node == nil {
 		return node
 	}
 
+	// 声明节点访问标记 Map
+	// 除了访问标记外，该 Map 还有另外一个作用
+	// 那就是将源节点和克隆节点进行映射
 	visited := make(map[*Node]*Node)
+	// 克隆起始节点 (将起始节点标记为已访问)
 	visited[node] = &Node{node.Val, []*Node{}}
+
+	// 将起始节点添加到队列中
 	queue := []*Node{node}
 
 	for len(queue) > 0 {
+		// 从队列中取出第一个节点
 		top := queue[0]
+		// 第一个节点出队
 		queue = queue[1:]
 
 		for _, v := range top.Neighbors {
 			if _, ok := visited[v]; !ok {
+				// 如果邻居节点还未被克隆过 (未被访问过)
+				// 克隆邻居节点 (标记为已访问)
 				visited[v] = &Node{v.Val, []*Node{}}
+
+				// 将邻居节点添加到队列中
 				queue = append(queue, v)
 			}
+
+			// 更新当前节点的邻居节点
 			visited[top].Neighbors = append(visited[top].Neighbors, visited[v])
 		}
 	}
 
+	// 返回起始节点对应的克隆新节点
 	return visited[node]
 }
--- 0200.Number-of-Islands/Solution.go
+++ 0200.Number-of-Islands/Solution.go
@@ -30,3 +30,48 @@ func dfs(grid [][]byte, row, col int) {
 	dfs(grid, row, col-1) // <- 方向递归
 	dfs(grid, row, col+1) // -> 方向递归
 }
+
+func numIslands2(grid [][]byte) int {
+	res := 0
+
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if grid[i][j] == '1' {
+				// 如果找到一块陆地，以该坐标为中心，上下左右四个方向继续探索
+				res++
+				bfs(grid, i, j)
+			}
+		}
+	}
+
+	return res
+}
+
+func bfs(grid [][]byte, i, j int) {
+	// 将当前的起始坐标加到队列
+	// 这里的队列使用二维数组表示
+	queue := [][2]int{}
+	queue = append(queue, [2]int{i, j})
+
+	for len(queue) > 0 {
+		// 从队列中取出第一个坐标
+		i, j := queue[0][0], queue[0][1]
+		// 第一个坐标出队
+		queue = queue[1:]
+
+		// 如果坐标未越界 并且 当前坐标是陆地
+		if i >= 0 && i < len(grid) && j >= 0 && j < len(grid[0]) && grid[i][j] == '1' {
+			// 已经探索过的坐标标记为 0, 避免重复计算
+			grid[i][j] = '0'
+
+			// ⬆ 方向继续探索
+			queue = append(queue, [2]int{i - 1, j})
+			// ⬇ 方向继续探索
+			queue = append(queue, [2]int{i + 1, j})
+			// <- 方向继续探索
+			queue = append(queue, [2]int{i, j - 1})
+			// -> 方向继续探索
+			queue = append(queue, [2]int{i, j + 1})
+		}
+	}
+}
