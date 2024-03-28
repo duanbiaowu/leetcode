134dd55ab51694dd17a82ae50331d4bb50f8f3be
duanbiaowu <codean.net@gmail.com>
2024-03-28 11:57:52 +0800

    docs: archive

--- 0020.Valid-Parentheses/Solution.go
+++ 0020.Valid-Parentheses/Solution.go
@@ -1,22 +1,27 @@
 package leetcode
 
 func isValid(s string) bool {
-	n := len(s)
-	if n&1 == 1 {
+	// 如果 s 的长度为奇数，肯定不是有效括号
+	if len(s)&1 == 1 {
 		return false
 	}
 
+	// 将左右边界字符进行分组并形成映射
 	charMap := map[byte]byte{
 		'(': ')',
 		'{': '}',
 		'[': ']',
 	}
+
+	// 初始化栈
 	var stack []byte
 
-	for i := 0; i < n; i++ {
+	for i := range s {
 		if _, ok := charMap[s[i]]; ok {
+			// 左边界组的字符直接入栈
 			stack = append(stack, s[i])
 		} else {
+			// 右边界组的字符与栈顶元素进行比较
 			index := len(stack) - 1
 			if len(stack) == 0 || s[i] != charMap[stack[index]] {
 				return false
@@ -25,5 +30,7 @@ func isValid(s string) bool {
 		}
 	}
 
+	// 此时栈内可能还存在一些左边界字符
+	// 所以需要判断栈是否为空
 	return len(stack) == 0
 }
--- 0071.Simplify-Path/Solution.go
+++ 0071.Simplify-Path/Solution.go
@@ -6,12 +6,22 @@ import (
 
 func simplifyPath(path string) string {
 	var stack []string
-	for _, component := range strings.Split(path, "/") {
-		if component != "" && component != "." && component != ".." {
-			stack = append(stack, component)
-		} else if component == ".." && len(stack) > 0 {
+
+	for _, p := range strings.Split(path, "/") {
+		// 无效路径字符，直接跳过
+		if p == "" || p == "." {
+			continue
+		}
+
+		// 目录名称/文件名称 入栈
+		if p != ".." {
+			stack = append(stack, p)
+		} else if p == ".." && len(stack) > 0 {
+			// 如果是上级目录 ..
+			// 将栈顶元素出栈
 			stack = stack[:len(stack)-1]
 		}
 	}
+
 	return "/" + strings.Join(stack, "/")
 }
