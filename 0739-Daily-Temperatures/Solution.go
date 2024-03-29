862aac757fd5b6d9f2d56b4ee00bb5f78555108b
duanbiaowu <codean.net@gmail.com>
2024-03-29 13:10:45 +0800

    docs: archive

--- 0739-Daily-Temperatures/Solution.go
+++ 0739-Daily-Temperatures/Solution.go
@@ -32,14 +32,37 @@ func dailyTemperatures(temperatures []int) []int {
 func dailyTemperatures2(temperatures []int) []int {
 	res := make([]int, len(temperatures))
 
+	// 1. 初始化单调栈
+	// 因为题目要求寻找 “下一个” 更高的温度对应的索引 (间隔天数)
+	// 所以栈内存储索引
 	var stack []int
+
+	// 2. 遍历数组
 	for i := range temperatures {
+		// 当前栈内索引对应的温度依次递减，符合单调递减的特性
+		// 也就是说，如果一个索引出现在栈内
+		// 说明还未找到该索引对应的温度 的  “下一个” 更高的温度
+
+		// 3. 维护单调递减性
+		// 如果当前元素温度 大于 栈顶元素温度
+		// 说明当前单调递减性已经被破坏
+		// 那么当前元素就是  “下一个” 更高的温度
 		for len(stack) > 0 && temperatures[i] > temperatures[stack[len(stack)-1]] {
+			// 4. 处理具体逻辑
+
+			// 获取栈顶元素索引
 			preIndex := stack[len(stack)-1]
+			// 栈顶元素出栈
 			stack = stack[:len(stack)-1]
+
+			// 更新栈顶元素索引对应的温度 的  “下一个” 更高的温度
+			// 因为题目要求寻找 “下一个”  更高的温度在几天后
+			// 所以这里使用当前索引 减去 栈顶索引
+			// 得出的差值就是中间的间隔天数
 			res[preIndex] = i - preIndex
 		}
 
+		// 将当前元素的索引入栈
 		stack = append(stack, i)
 	}
 
