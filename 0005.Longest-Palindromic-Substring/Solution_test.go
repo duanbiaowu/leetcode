5730fba1a02a3055dd929a59bade6c5a1be63386
duanbiaowu <codean.net@gmail.com>
2024-04-18 18:12:57 +0800

    docs: archive

--- 0005.Longest-Palindromic-Substring/Solution_test.go
+++ 0005.Longest-Palindromic-Substring/Solution_test.go
@@ -46,7 +46,7 @@ func Test_longestPalindrome(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := longestPalindrome(tt.args.s); got != tt.want {
+			if got := longestPalindromeMemo(tt.args.s); got != tt.want {
 				t.Errorf("longestPalindrome() = %v, want %v", got, tt.want)
 			}
 		})
