32132d29f958c908c675981ac14522f5cb6e604b
duanbiaowu <codean.net@gmail.com>
2024-12-26 08:00:29 +0800

    refactor: solution

--- 0118.Pascals-Triangle/Solution.go
+++ 0118.Pascals-Triangle/Solution.go
@@ -1,17 +1,24 @@
 package leetcode
 
 func generate(numRows int) [][]int {
-	if numRows <= 0 {
+	if numRows < 1 {
 		return [][]int{}
 	}
+
 	res := make([][]int, numRows)
-	for i := 0; i < numRows; i++ {
-		res[i] = make([]int, i+1)
-		res[i][0] = 1
-		res[i][i] = 1
-		for j := 1; j < i; j++ {
-			res[i][j] = res[i-1][j] + res[i-1][j-1]
+
+	for row := 0; row < numRows; row++ {
+		// 初始化每一行的数组
+		res[row] = make([]int, row+1)
+		// 第一列和最后一列都是1
+		res[row][0] = 1
+		res[row][row] = 1
+
+		// 计算当前行从 第 2 列到倒数第 2 列 的值
+		for col := 1; col < row; col++ {
+			res[row][col] = res[row-1][col-1] + res[row-1][col]
 		}
 	}
+
 	return res
 }
--- 0347-Top-K-Frequent-Elements/Solution.go
+++ 0347-Top-K-Frequent-Elements/Solution.go
@@ -67,32 +67,40 @@ func topKFrequent2(nums []int, k int) []int {
 		return nums
 	}
 
+	// 统计每个数字出现的次数
+	// key: 数字, value: 出现的次数
+	// 例如: nums = [1, 1, 1, 2, 2, 3, 4, 5],
+	// numMap = {1: 3, 2: 2, 3: 1, 4: 1, 5: 1}
 	numMap := make(map[int]int)
 	for i := range nums {
 		numMap[nums[i]]++
 	}
 
+	// 按照数字出现的次数进行桶排序
+	// key: 出现的次数, value: 出现次数为 key 的数字列表
+	// 例如: cntMap = {3: [1], 2: [2], 1: [3, 4, 5]}
 	cntMap := make(map[int][]int)
-	for val, cnt := range numMap {
-		if _, ok := cntMap[cnt]; ok {
-			cntMap[cnt] = append(cntMap[cnt], val)
-		} else {
-			cntMap[cnt] = []int{val}
-		}
+	for num, cnt := range numMap {
+		cntMap[cnt] = append(cntMap[cnt], num)
 	}
 
-	res := make([]int, k)
-	pos := 0
+	res := []int{}
+
+	// 题目要求: 1 <= nums.length <= 105
+	// 从后往前遍历, 保证出现次数多的数字在前面
 	for i := len(nums); i > 0; i-- {
-		if _, ok := cntMap[i]; ok {
-			for j := range cntMap[i] {
-				res[pos] = cntMap[i][j]
-				pos++
-				if pos == k {
-					return res
-				}
+		if _, ok := cntMap[i]; !ok {
+			continue
+		}
+
+		for _, num := range cntMap[i] {
+			res = append(res, num)
+			// 题目要求返回前 k 个出现次数最多的数字
+			if len(res) == k {
+				return res
 			}
 		}
 	}
+
 	return res
 }
