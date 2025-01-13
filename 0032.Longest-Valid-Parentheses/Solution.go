71404741a1e324791b7784cb708d142818300828
duanbiaowu <codean.net@gmail.com>
2025-01-13 20:50:14 +0800

    refactor: solution

--- 0032.Longest-Valid-Parentheses/Solution.go
+++ 0032.Longest-Valid-Parentheses/Solution.go
@@ -2,21 +2,35 @@ package leetcode
 
 // https://leetcode-cn.com/problems/longest-valid-parentheses/solution/zui-chang-you-xiao-gua-hao-by-leetcode-solution/
 func longestValidParentheses(s string) int {
-	res, left, right := 0, 0, 0
+	res := 0
+	left, right := 0, 0
 
-	for i := 0; i < len(s); i++ {
-		if s[i] == '(' {
+	// 贪心算法:
+	// 从前向后遍历
+	// 逐个计算以每个字符结尾的有效括号长度
+	for _, c := range s {
+		if c == '(' {
 			left++
 		} else {
 			right++
 		}
+
 		if left == right {
-			res = max(res, right*2)
+			res = max(res, left<<1)
 		} else if right > left {
+			// 当右括号数量大于左括号数量时
+			// 说明当前的左右边界组成的子括号不符合规则
+			// 这时就置为 0, 开始重新计算
 			left, right = 0, 0
 		}
 	}
 
+	// 为什么还需要再次从头向前遍历？
+	// 兼容极端的边界情况 (遍历时左括号数量始终大于右括号数量)
+	// 例如: (()
+	// 此时正确的有效括号长度应该是 2
+	// 如果只有从前向后遍历，那么计算出的结果是 0
+	// 所以需要从头向前再遍历一次
 	left, right = 0, 0
 	for i := len(s) - 1; i >= 0; i-- {
 		if s[i] == '(' {
@@ -24,8 +38,9 @@ func longestValidParentheses(s string) int {
 		} else {
 			right++
 		}
+
 		if left == right {
-			res = max(res, left*2)
+			res = max(res, left<<1)
 		} else if left > right {
 			left, right = 0, 0
 		}
@@ -36,17 +51,25 @@ func longestValidParentheses(s string) int {
 
 func longestValidParenthesesStack(s string) int {
 	res := 0
-	// 边界情况: 始终保持栈底元素为当前已经遍历过的元素中「最后一个没有被匹配的右括号的下标」
+	// 边界情况: 始终保持栈底元素为当前已经遍历过的元素中「最后一个没有被匹配右括号的 左括号下标」
 	stack := []int{-1}
 
 	for i := 0; i < len(s); i++ {
+		// 遇到左括号时直接入栈
 		if s[i] == '(' {
 			stack = append(stack, i)
 		} else {
+			// 遇到右括号时先将栈顶元素出栈，表示匹配当前的左括号
 			stack = stack[:len(stack)-1]
 			if len(stack) == 0 {
+				// 如果栈顶元素出栈后，栈变为空，说明之前栈内的元素只有一个 -1
+				// 也就是当前的右括号，无法正常匹配
+				// 既然无法匹配，那就直接将当前的右括号加入到栈内
+				// 更新「最后一个没有被匹配右括号的 左括号下标」
 				stack = append(stack, i)
 			} else {
+				// 如果栈顶元素出栈后，栈不为空
+				// 当前有括号的索引减去栈顶元素就等于「以该右括号为结尾的最长有效括号的长度」
 				res = max(res, i-stack[len(stack)-1])
 			}
 		}
@@ -57,25 +80,37 @@ func longestValidParenthesesStack(s string) int {
 
 func longestValidParenthesesDp(s string) int {
 	res := 0
+
+	// 状态转移方程:
+	// 1. 当 s[i-1] = '(' && s[i] = ')'
+	//    例如 ..()() 这种形式
+	//    dp[i] = dp[i−2] + 2
+	// 2. 当 s[i-1] = ')' && s[i] = ')"
+	//    例如 ..)) 这种形式
+	//    如果 s[ i - dp[i-1] - 1 ] = '('
+	//    那么 dp[i] = dp[i-1] + dp[ i - dp[i-1] - 2 ] + 2
 	dp := make([]int, len(s))
 
 	for i := 1; i < len(s); i++ {
-		if s[i] == ')' {
-			if s[i-1] == '(' {
-				if i >= 2 {
-					dp[i] = dp[i-2] + 2
-				} else {
-					dp[i] = 2
-				}
-			} else if i-dp[i-1] > 0 && s[i-dp[i-1]-1] == '(' {
-				if i-dp[i-1] >= 2 {
-					dp[i] = dp[i-1] + dp[i-dp[i-1]-2] + 2
-				} else {
-					dp[i] = dp[i-1] + 2
-				}
-			}
-			res = max(res, dp[i])
+		if s[i] != ')' {
+			continue
 		}
+
+		if s[i-1] == '(' {
+			if i >= 2 {
+				dp[i] = dp[i-2] + 2
+			} else {
+				dp[i] = 2
+			}
+		} else if i-dp[i-1] > 0 && s[i-dp[i-1]-1] == '(' {
+			if i-dp[i-1] >= 2 {
+				dp[i] = dp[i-1] + dp[i-dp[i-1]-2] + 2
+			} else {
+				dp[i] = dp[i-1] + 2
+			}
+		}
+
+		res = max(res, dp[i])
 	}
 
 	return res
--- 0216.Combination-Sum3/Solution.go
+++ 0216.Combination-Sum3/Solution.go
@@ -6,34 +6,33 @@ func combinationSum3(k int, n int) [][]int {
 	var res [][]int
 	var path []int
 
-	// 直接使用内部函数实现回溯
-	// 简化外部函数实现时需要传递多个参数的方式
-	var backtrack func(cur, rest int)
-	backtrack = func(cur, rest int) {
-		if len(path) == k && rest == 0 {
-			res = append(res, append([]int{}, path...))
-			return
-		}
-
-		// 剪枝:
-		// 1. 当前选取的数字个数即使再怎么加其他数字，也不可能组成目标数字 n (小于关系)
-		// 2. 当前选取的数字总和已经超过目标数字 n (大于关系)
-		if len(path)+10-cur < k || rest < 0 {
-			return
-		}
-
-		// 跳过当前数字
-		backtrack(cur+1, rest)
-
-		// 选择当前数字
-		path = append(path, cur)
-		backtrack(cur+1, rest-cur)
-		path = path[:len(path)-1]
-	}
-
-	backtrack(1, n)
+	backtrack(k, 1, n, &path, &res)
 
 	return res
+}
+
+func backtrack(k, start, target int, path *[]int, res *[][]int) {
+	// 回溯结束条件
+	if len(*path) == k && target == 0 {
+		*res = append(*res, append([]int{}, *path...))
+		return
+	}
+
+	// 剪枝条件
+	// 1. 当前选取的数字总和已经大于 n (所以 target 为负数)
+	// 2. 当前选择的数字总和太小了, 即使再添加其他数字，长度也无法满足要求 (小于 k)
+	//     所以没有继续回溯的必要了
+	if target < 0 || len(*path)-start+10 < k {
+		return
+	}
+
+	// 不选择当前数字
+	backtrack(k, start+1, target, path, res)
+
+	// 选择当前数字
+	*path = append(*path, start)
+	backtrack(k, start+1, target-start, path, res)
+	*path = (*path)[:len(*path)-1]
 }
 
 // 也可以直接复用 combinationSum2 方法
