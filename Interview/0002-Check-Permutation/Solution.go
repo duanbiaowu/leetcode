95a7c0694000c956cfda798e29b716cadd37b557
duanbiaowu <codean.net@gmail.com>
2025-02-07 22:42:57 +0800

    refactor: solution

--- Interview/0002-Check-Permutation/Solution.go
+++ Interview/0002-Check-Permutation/Solution.go
@@ -1,19 +1,22 @@
 package leetcode
 
 func CheckPermutation(s1 string, s2 string) bool {
-	n := len(s1)
-	if n != len(s2) {
+	if len(s1) != len(s2) {
 		return false
 	}
-	var counter [128]int
-	for i := 0; i < n; i++ {
-		counter[s1[i]-'0']++
-		counter[s2[i]-'0']--
+
+	cnt := [128]int{}
+
+	for i := range s1 {
+		cnt[s1[i]-'0']++
+		cnt[s2[i]-'0']--
 	}
-	for i := 0; i < n; i++ {
-		if counter[s1[i]-'0'] != 0 {
+
+	for i := range s1 {
+		if cnt[s1[i]-'0'] != 0 {
 			return false
 		}
 	}
+
 	return true
 }
--- Interview/0003-Replace-Spaces/Solution.go
+++ Interview/0003-Replace-Spaces/Solution.go
@@ -4,18 +4,19 @@ import "strings"
 
 func replaceSpaces(S string, length int) string {
 	var buf strings.Builder
-	space := 0
+
+	spaces := 0
 	for i := 0; i < length; i++ {
 		if S[i] == ' ' {
-			space++
+			spaces++
 		}
 	}
-	buf.Grow(space*2 + length)
+
+	buf.Grow(spaces<<1 + length)
+
 	for i := 0; i < length; i++ {
 		if S[i] == ' ' {
-			buf.WriteByte('%')
-			buf.WriteByte('2')
-			buf.WriteByte('0')
+			buf.WriteString("%20")
 		} else {
 			buf.WriteByte(S[i])
 		}
